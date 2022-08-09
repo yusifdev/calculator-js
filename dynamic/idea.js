cd14d496d9d93e472cd4dd0470153438ab437aac
yusifdev <mirzequliyevyusif@gmail.com>
2022-08-09 17:33:06 +0400

    --last version

---
+++ dynamic/idea.js
@@ -0,0 +1,62 @@
+var screen = document.getElementsByClassName("screen")[0]
+
+function show1(){
+    screen.innerHTML += 1
+}
+function show2(){
+    screen.innerHTML += 2
+}
+function show3(){
+    screen.innerHTML += 3
+}
+function show4(){
+    screen.innerHTML += 4
+}
+function show5(){
+    screen.innerHTML += 5
+}
+function show6(){
+    screen.innerHTML += 6
+}
+function show7(){
+    screen.innerHTML += 7
+}
+function show8(){
+    screen.innerHTML += 8
+}
+function show9(){
+    screen.innerHTML += 9
+}
+function show0(){
+    screen.innerHTML += 0
+}
+function showbolme(){
+    screen.innerHTML += "/"
+}
+function showvurma(){
+    screen.innerHTML += "*"
+}
+function showminus(){
+    screen.innerHTML += "-"
+}
+function showpoint(){
+    screen.innerHTML += "."
+}
+function showequal(){
+    screen.innerHTML = (eval(screen.innerHTML))
+}
+function showplus(){
+    screen.innerHTML += "+"
+}
+function showc(){
+    screen.innerHTML = " "
+}
+function showsolmoterize(){
+    screen.innerHTML += "("
+}
+function showsagmoterize(){
+    screen.innerHTML += ")"
+}
+function showce(){
+    screen.innerHTML = (screen.innerHTML.slice(0, -1))
+}
